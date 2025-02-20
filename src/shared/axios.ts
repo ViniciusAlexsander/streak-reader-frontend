7026478d07a1ff14bae39851d342929180372124
Vinicius Marinho <viniciusalexsander1@gmail.com>
2025-02-20 00:44:01 -0300

    fix: add env

--- src/shared/axios.ts
+++ src/shared/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAccessToken } from "./tokenUtils";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.BACKEND_URL,
   withCredentials: true,
 });
 
