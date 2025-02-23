14196c0d4d99ef6062510204cf125971f33ff978
Vinicius Marinho <viniciusalexsander1@gmail.com>
2025-02-23 01:28:40 -0300

    feat: add empty feedback

--- src/app/(private)/admin/page.tsx
+++ src/app/(private)/admin/page.tsx
@@ -91,65 +91,72 @@ export default function Page() {
           <Spinner size={{ base: "lg" }} />
         </Flex>
       )}
-      {!loading &&
-        users.map((user, index) => (
-          <Flex
-            gap={{ base: 5, md: 10 }}
-            alignItems="center"
-            justifyContent="center"
-            width="full"
-          >
-            <Text
-              fontSize={{ base: "2xl", md: "4xl" }}
-              width="15%"
-              textAlign="center"
+      {!loading && users.length === 0 && (
+        <Text fontSize={{ base: "2xl" }} fontWeight="bold" mt={"40"}>
+          Nenhum usuário encontrado, tente alterar o filtro
+        </Text>
+      )}
+      {!loading && users.length > 0 && (
+        <>
+          {users.map((user, index) => (
+            <Flex
+              gap={{ base: 5, md: 10 }}
+              alignItems="center"
+              justifyContent="center"
+              width="full"
             >
-              {userPosition(index)}º
-            </Text>
-            <Box
-              key={user.email}
-              borderWidth="1px"
-              borderColor="MenuText"
-              borderRadius="md"
-              padding={{ base: 2, md: 5 }}
-              width="85%"
-              maxWidth={{ base: "60", md: "96" }}
-            >
-              <Flex gap={2}>
-                <Text fontWeight="bold">Streak atual:</Text>
-                <Text>{user.actualStreak}</Text>
-              </Flex>
-              <Flex gap={2}>
-                <Text fontWeight="bold">Total de leituras:</Text>
-                <Text>{user._count.readPosts}</Text>
-              </Flex>
-
-              <Flex gap={2}>
-                <Text fontWeight="bold">Record de streak:</Text>
-                <Text>{user.recordStreak}</Text>
-              </Flex>
-              <Flex
-                gap={{ base: 0, md: 2 }}
-                flexDirection={{ base: "column", md: "row" }}
+              <Text
+                fontSize={{ base: "2xl", md: "4xl" }}
+                width="15%"
+                textAlign="center"
               >
-                <Text fontWeight="bold">Usuário:</Text>
-                <Text>{user.email}</Text>
-              </Flex>
-            </Box>
-          </Flex>
-        ))}
+                {userPosition(index)}º
+              </Text>
+              <Box
+                key={user.email}
+                borderWidth="1px"
+                borderColor="MenuText"
+                borderRadius="md"
+                padding={{ base: 2, md: 5 }}
+                width="85%"
+                maxWidth={{ base: "60", md: "96" }}
+              >
+                <Flex gap={2}>
+                  <Text fontWeight="bold">Streak atual:</Text>
+                  <Text>{user.actualStreak}</Text>
+                </Flex>
+                <Flex gap={2}>
+                  <Text fontWeight="bold">Total de leituras:</Text>
+                  <Text>{user._count.readPosts}</Text>
+                </Flex>
 
-      <PaginationRoot
-        count={pagination.totalCount}
-        pageSize={pagination.pageSize}
-        onPageChange={(e) => setCurrentPage(e.page)}
-      >
-        <HStack wrap="wrap">
-          <PaginationPrevTrigger />
-          <PaginationItems />
-          <PaginationNextTrigger />
-        </HStack>
-      </PaginationRoot>
+                <Flex gap={2}>
+                  <Text fontWeight="bold">Record de streak:</Text>
+                  <Text>{user.recordStreak}</Text>
+                </Flex>
+                <Flex
+                  gap={{ base: 0, md: 2 }}
+                  flexDirection={{ base: "column", md: "row" }}
+                >
+                  <Text fontWeight="bold">Usuário:</Text>
+                  <Text>{user.email}</Text>
+                </Flex>
+              </Box>
+            </Flex>
+          ))}
+          <PaginationRoot
+            count={pagination.totalCount}
+            pageSize={pagination.pageSize}
+            onPageChange={(e) => setCurrentPage(e.page)}
+          >
+            <HStack wrap="wrap">
+              <PaginationPrevTrigger />
+              <PaginationItems />
+              <PaginationNextTrigger />
+            </HStack>
+          </PaginationRoot>
+        </>
+      )}
     </Stack>
   );
 }
